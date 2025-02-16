0d75425d728d7ff0b1425fd2993262b77dd92fd4
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-02-15 23:29:37 -0800

    create autogrow function

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import Trash from "../icons/Trash";
 
@@ -6,11 +7,25 @@ const NoteCard = ({ note }) => {
   let position = JSON.parse(note.position);
   const colors = JSON.parse(note.colors);
 
+  const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    autoGrow(textAreaRef);
+  }, []);
+
+  const autoGrow = (textAreaRef) => {
+    const { current } = textAreaRef;
+    current.style.height = "auto"; // Reset the height
+    current.style.height = current.scrollHeight + "px"; // Set the new height
+  };
+
   return (
     <div
       className="card"
       style={{
         backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
       }}
     >
       <div
@@ -22,8 +37,10 @@ const NoteCard = ({ note }) => {
 
       <div className="card-body">
         <textarea
+          ref={textAreaRef}
           style={{ color: colors.colorText }}
           defaultValue={body}
+          onInput={() => autoGrow(textAreaRef)}
           name=""
           id=""
         ></textarea>
