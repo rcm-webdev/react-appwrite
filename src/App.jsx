efd2d78dff474398fe2186b9f3679289442509d9
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-02-15 19:34:32 -0800

    update css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-export const App = () => {
+import NotesPage from "./pages/NotesPage";
+
+const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div id="app">
+      <NotesPage />
     </div>
   );
 };
