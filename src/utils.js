7108d0a700168331aabdcc87c83df631de3b2bce
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-02-16 12:46:41 -0800

    update zindex of selectedcard

--- src/utils.js
+++ src/utils.js
@@ -13,3 +13,13 @@ export const autoGrow = (textAreaRef) => {
   current.style.height = "auto"; // Reset the height
   current.style.height = current.scrollHeight + "px"; // Set the new height
 };
+
+export const setZIndex = (selectedCard) => {
+  selectedCard.style.zIndex = 999;
+
+  Array.from(document.getElementsByClassName("card")).forEach((card) => {
+    if (card !== selectedCard) {
+      card.style.zIndex = selectedCard.style.zIndex - 1;
+    }
+  });
+};
