cac7b4fac061965ca882da1dd23464ecf58a6fd7
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-02-15 20:01:26 -0800

    render notecards on notepage

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,5 +1,14 @@
+import NoteCard from "../components/NoteCard.jsx";
+import { fakeData as notes } from "../assets/fakeData.js";
+
 const NotesPage = () => {
-  return <div>NotesPage</div>;
+  return (
+    <div>
+      {notes.map((note) => (
+        <NoteCard key={note.$id} note={note} />
+      ))}
+    </div>
+  );
 };
 
 export default NotesPage;
